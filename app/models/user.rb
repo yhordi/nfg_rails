dfb61ebac08313c33378c5e2f7e084b3e4dd2af9
Jordan Kamin <jordanakamin@gmail.com>
2014-11-24 18:03:01 -0800

    Tests the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, :presence => true, :uniqueness => true
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :username, :presence => true, :uniqueness => true
+  validates :username, length: { minimum: 6 }
 end
