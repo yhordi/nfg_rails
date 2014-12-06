fcbb51d25b60d18d3b8bdf84e6d64d3d0a9465c8
Jordan Kamin <jordanakamin@gmail.com>
2014-12-06 11:05:11 -0800

    Gets appropriate data from google calendar api. 	Need to format date.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,9 +1,10 @@
+require 'json'
 
 class CalendarsController < ApplicationController
   def index
-    p ENV['GCAL_KEY']
   @response = HTTParty.get("https://www.googleapis.com/calendar/v3/calendars/nebulaforcego%40gmail.com/events?key=#{ENV['GCAL_KEY']}")
-  p @response
+  @parse = JSON.parse(@response.body).as_json
+  p @parse
   end
 
 end
