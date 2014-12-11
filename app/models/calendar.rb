0d09e97b075dfcaa93f577be21bb4ac624c9b6c9
Jordan Kamin <jordanakamin@gmail.com>
2014-12-11 12:45:32 -0800

    Fixes a bug causing expired calendar events to show up.

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,7 +2,7 @@ class Calendar < ActiveRecord::Base
   validate :time_cannot_be_in_the_past,
 
   def time_cannot_be_in_the_past
-    if time.present? && time <= Date.today
+    if time.present? && time <= Time.now
       errors.add(:time, "can't be in the past")
     end
   end
