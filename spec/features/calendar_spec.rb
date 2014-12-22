d86a796a681d1a9efa59289770b27a5d72bc8629
Jordan Kamin <jordanakamin@gmail.com>
2014-12-22 08:54:00 -0800

    Calendars feature complete.

--- spec/features/calendar_spec.rb
+++ spec/features/calendar_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Calendar", js: true do
+describe "Calendar with empty database", js: true do
   let!(:user) { FactoryGirl.create :user }
   context "with a logged in user" do
     before(:each) do
@@ -18,8 +18,27 @@ describe "Calendar", js: true do
         expect(page).to have_button("Create")
       end
     end
-    xit "should display a calendar event and a refresh button" do
-
+  end
+end
+describe "Calendar with items in database", js: true do
+  let!(:user) { FactoryGirl.create :user }
+  let!(:calendar) { FactoryGirl.create :calendar }
+  let(:attributes) { FactoryGirl.attributes_for :calendar }
+  context "with a logged in user" do
+    before(:each) do
+        visit new_session_path
+        fill_in "Username", with: user.username
+        fill_in "Password", with: user.password
+        click_on "Log In"
+        visit calendars_path
+    end
+    it "should display a calendar event" do
+      expect(page).to have_content(calendar.summary)
+    end
+    it "should display a refresh button" do
+      within("form#new_calendar") do
+        expect(page).to have_button("Refresh")
+      end
     end
   end
 end
