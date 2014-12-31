5a5113b54bddf185669b9780c0758b13adc9c191
Jordan Kamin <jordanakamin@gmail.com>
2014-12-31 13:00:28 -0800

    Gets appropriate response from YouTube api.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,9 @@
 class VideosController < ApplicationController
 
   def index
+    @response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCbmYMZyMPkoLUFRom02z-9w&key=#{ENV['GCAL_KEY']}")
+    p @response
+
   end
 
   def show
