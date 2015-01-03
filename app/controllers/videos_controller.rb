e3589496a4e6c54745059ab693598c5068e1babc
Jordan Kamin <jordanakamin@gmail.com>
2015-01-03 12:22:39 -0800

    Getting videos in the page, but not all of them.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,10 @@
 class VideosController < ApplicationController
 
   def index
-    @response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCbmYMZyMPkoLUFRom02z-9w&key=#{ENV['GCAL_KEY']}")
-    p @response
-
+    @response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=id&channelId=UCbmYMZyMPkoLUFRom02z-9w&key=#{ENV['GCAL_KEY']}")
+    @parse = JSON.parse(@response.body)
   end
-
   def show
   end
 end
+
