382aa8e4d6ef6246dc69a1fa360cc2d3ac227765
Jordan Kamin <jordanakamin@gmail.com>
2015-01-04 23:10:17 -0800

    Makes the correct api call and returns all of the videos.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,11 +5,13 @@ class VideosController < ApplicationController
   end
 
   def create
-    @response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=id&channelId=UCbmYMZyMPkoLUFRom02z-9w&key=#{ENV['GCAL_KEY']}")
+    Video.delete_all
+    @response = HTTParty.get("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUbmYMZyMPkoLUFRom02z-9w&key=#{ENV['GCAL_KEY']}")
     @parse = JSON.parse(@response.body)
     @parse["items"].each do |item|
-      if item["id"]["kind"]  == "youtube#video"
-        Video.create(link: "http://youtube.com/embed/#{item["id"]["videoId"]}")
+      video_info = item["snippet"]["resourceId"]
+      if video_info["kind"] == "youtube#video"
+        Video.create(link: "http://youtube.com/embed/#{video_info["videoId"]}")
       end
     end
     redirect_to '/videos'
