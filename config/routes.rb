40eddce94f41c103f6c85bc5cbaacae9dada4f64
Jordan Kamin <apprentice@DBC-infinity-too.local>
2015-02-18 11:29:56 -0800

    Aliases sessions/new to go.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :sessions, only: [:create, :new, :destroy, :show]
+  resources :sessions, only: [:create, :destroy, :show]
+  get 'go', to: 'sessions#new'
   resources :users, only: [:create, :show]
   resources :posts
   resources :calendars, only: [:index, :create]
