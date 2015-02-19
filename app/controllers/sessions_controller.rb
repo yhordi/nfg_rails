bd83113616bc808a0498213f801b654bc1571360
Jordan Kamin <apprentice@DBC-infinity-too.local>
2015-02-18 16:40:49 -0800

    creates password update functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       login(user)
       redirect_to root_path
     else
-      redirect_to new_session_path, :flash => { :error => "Incorrect user name or password. Please try again." }
+      redirect_to go_path, :flash => { :error => "Incorrect user name or password. Please try again." }
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,21 @@ class UsersController < ApplicationController
     if session[:id] == nil
       redirect_to root_path
     else
-      @user = User.find params[:id]
+      @user = User.find(params[:id])
     end
   end
 
   def update
-    user = User.find(params[:id])
-    user.update_attributes
-    redirect_to users_show_path
+    @user = User.find(params[:id])
+    p "*"*50
+    p params
+    if @user.authenticate(params[:old_password]) == @user
+      @user.password = params[:user][:password]
+      @user.save!
+      redirect_to user_path
+    else
+     redirect_to user_path, :flash => { :error => "Your new password was not saved." }
+    end
   end
 
   private
