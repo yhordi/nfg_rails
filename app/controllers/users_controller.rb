d1c9061d526ca296eeeed53e770552cf2b3402f3
Jordan Kamin <apprentice@DBC-infinity-too.local>
2015-02-18 16:46:24 -0800

    creates flash notice on successful update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     if @user.authenticate(params[:old_password]) == @user
       @user.password = params[:user][:password]
       @user.save!
+      flash[:notice] = "Password updated"
       redirect_to user_path
     else
      redirect_to user_path, :flash => { :error => "Your new password was not saved." }
