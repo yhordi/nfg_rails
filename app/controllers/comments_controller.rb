a148bb69e5c9941e7b76da4a14d0863e6713dbae
Jordan Kamin <jordanakamin@gmail.com>
2015-03-25 11:09:01 -0700

    builds new comment form, working on create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,19 @@
 class CommentsController < ApplicationController
-  def index
-    ap params
-    @comments = Post.find(params[:post_id]).comments
-    ap @comments
-    # @post_comments(@comments)
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    p params
+    comment = Comment.new
+    comment.name = params[:comment][:name]
+    comment.body = params[:comment][:body]
+    if comment.valid?
+      comment.save!
+      redirect_to post_path(params[:post_id])
+    else
+      flash.errors.full_messages
+      redirect_to post_path(params[:post_id])
+    end
   end
 end
