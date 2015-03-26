4ac171f5292dad5e80b904a1164d9468703fbcca
Jordan Kamin <jordanakamin@gmail.com>
2015-03-26 15:19:29 -0700

    :star: finishes feature testing for comments

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe 'Comment', js: true do
   let!(:article) { FactoryGirl.create :post }
   let!(:comment) { FactoryGirl.create :comment }
-  before(:each) do
-    visit post_path(article)
-  end
-  context "a guest user on a post's page" do
+  let!(:user) { FactoryGirl.create :user }
+  describe "a guest user" do
+    before(:each) do
+      visit post_path(article)
+    end
     it "can see a comment" do
       expect(page).to have_content(comment.body)
     end
@@ -23,6 +24,20 @@ describe 'Comment', js: true do
       fill_in 'comment_name', with: name
       click_on 'Create Comment'
       expect(page).to have_content("Body can't be blank")
+    end
+  end
+  describe "a logged in user" do
+    before(:each) do
+      visit go_path
+      fill_in "Username", with: user.username
+      fill_in "Password", with: user.password
+      click_on "Log In"
+      visit post_path(article)
+    end 
+    it "can delete a comment" do
+      click_on "Delete comment"
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to_not have_content(comment.body)
     end
   end
 end
