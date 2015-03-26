0a330c4e17bf946aea0bde4c737fea016f1ec19c
Jordan Kamin <jordanakamin@gmail.com>
2015-03-26 15:09:39 -0700

    refactors comment feature tests, adds one

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -1,20 +1,26 @@
 require 'rails_helper'
 
 describe 'Comment', js: true do
-  let(:article) { FactoryGirl.create :post }
-  let(:comment) { FactoryGirl.create :comment }
+  let!(:article) { FactoryGirl.create :post }
+  let!(:comment) { FactoryGirl.create :comment }
   before(:each) do
     visit post_path(article)
   end
-  context "a guest user" do
-    it "can create a comment on a post" do
-      fill_in 'comment_name', with: comment.name
-      fill_in 'comment_body', with: comment.body
-      click_on 'Create Comment'
+  context "a guest user on a post's page" do
+    it "can see a comment" do
       expect(page).to have_content(comment.body)
     end
+    it "can create a comment on a post" do
+      body = "#fentylyfe"
+      name = "Fenty Soetarman"
+      fill_in 'comment_name', with: name
+      fill_in 'comment_body', with: body
+      click_on 'Create Comment'
+      expect(page).to have_content(body)
+    end
     it "renders an error on bad params" do
-      fill_in 'comment_name', with: comment.name
+      name = "Fenty Soetarman"
+      fill_in 'comment_name', with: name
       click_on 'Create Comment'
       expect(page).to have_content("Body can't be blank")
     end
