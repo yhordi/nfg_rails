7b960c97cdf88419011e1530653618b0677861ad
Jordan Kamin <jordanakamin@gmail.com>
2015-03-26 14:38:10 -0700

    tests delete method WIP

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -11,4 +11,15 @@ describe CommentsController do
       }.to change{ Comment.count }.by(1)
     end
   end
+  describe 'DELETE #destroy' do
+    it "deletes a comment in the database" do
+      expect {
+      delete :destroy, post_id: article.id, id: comment.id
+      }.to change{ Comment.count }.by(-1)
+    end
+    it "redirects to the post page" do
+      delete :destroy, post_id: article.id, id: comment.id
+      expect(response.status).to eq(302)
+    end
+  end
 end
