224d276d5e5e97fee82e290beb0e18435da08874
yhordi <jordan@devbootcamp.com>
2016-01-06 12:19:39 -0800

    WIP going to lunch

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,20 +7,15 @@ class VideosController < ApplicationController
   end
 
   def create
-    # Video.delete_all
     quota_guard_proxy
-    # p parse_json["items"]
-    ap get_youtube_channel
     resp = ApiResponse.new(name: 'youtube', body: get_youtube_channel)
     if resp.valid?
       resp.save!
     else
       old_repsonse = ApiResponse.find_by_name('youtube')
       old_repsonse.update!(body: resp.body)
+      ap parse(old_repsonse)
     end
-
-    ap parse(old_repsonse)
-
     # parse_json["items"].each do |item|
       # video_info = item["snippet"]["resourceId"]
       # if video_info["kind"] == "youtube#video"
