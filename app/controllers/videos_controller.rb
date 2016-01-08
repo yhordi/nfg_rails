95499e5889cf134d212f3c2228c8b5df3cec107c
yhordi <jordan@devbootcamp.com>
2016-01-08 14:23:32 -0800

    adds debug to VideosController#create

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -9,9 +9,11 @@ class VideosController < ApplicationController
   def create
     quota_guard_proxy
     # need to adjust get_youtube_channel method to get more videos
+    p "*"*50
     ap get_youtube_channel.contentLength
+    p "*"*50
     # ap parse(get_youtube_channel)
-    # ap resp = ApiResponse.new(name: 'youtube', body: get_youtube_channel)
+    # ap resp = ApiResponse.new(name: 'youtube', body: get_youtube_channel, content_length: get_youtube_channel.content_length.to_i)
     # hash["items"].each { |item| puts item["contentDetails"]["videoId"] }
 
     redirect_to '/videos'
