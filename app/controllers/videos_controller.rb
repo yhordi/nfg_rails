abfc2819d326f6aeddfa5feff61417d29a14f8da
yhordi <jordan@devbootcamp.com>
2016-01-08 18:49:53 -0800

    finishes test for first conditional branch in VideosController#create

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -9,9 +9,8 @@ class VideosController < ApplicationController
   def create
     quota_guard_proxy
     stored_youtube_response = ApiResponse.find_by_name('youtube')
-    ap get_youtube_channel
     if stored_youtube_response == nil
-      ApiResponse.create!(name: 'youtube',
+      stored_youtube_response = ApiResponse.create!(name: 'youtube',
                          body: get_youtube_channel,
                          content_length: youtube_content_length)
     elsif youtube_content_length != stored_youtube_response.content_length
@@ -23,7 +22,6 @@ class VideosController < ApplicationController
     response_hash["items"].each do |item|
       Video.create(link: "https://www.youtube.com/embed/#{item["contentDetails"]["videoId"]}")
     end
-
     redirect_to '/videos'
   end
 end
