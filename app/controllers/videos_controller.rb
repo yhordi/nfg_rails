06daa7c303d1e6d27a3e309357eb950b51ddee2e
yhordi <jordan@devbootcamp.com>
2016-01-08 16:31:43 -0800

    Adds conditional to VideosController#create

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -9,10 +9,16 @@ class VideosController < ApplicationController
   def create
     quota_guard_proxy
     stored_youtube_response = ApiResponse.find_by_name('youtube')
-    if youtube_content_length != stored_youtube_response.content_length
+
+    if stored_youtube_response == nil
+      ApiResponse.create(name: 'youtube',
+                         body: get_youtube_channel,
+                         content_length: content_length)
+    elsif youtube_content_length != stored_youtube_response.content_length
       ApiResponse.update!(body: get_youtube_channel, 
                           content_length: youtube_content_length)
     end
+
     response_hash = parse(stored_youtube_response)
     response_hash["items"].each do |item|
       Video.create(link: "https://www.youtube.com/embed/#{item["contentDetails"]["videoId"]}")
