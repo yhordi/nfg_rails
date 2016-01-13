f6be252794539f627147cf59e7b22a225815e404
yhordi <jordan@devbootcamp.com>
2016-01-13 10:20:16 -0800

    adds test for ApiResponse#get_youtube_channel

--- spec/models/api_response_spec.rb
+++ spec/models/api_response_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ApiResponse, :type => :model do
-  it { should validate_uniqueness_of :body }
-  it { should validate_uniqueness_of :name }
-  it { should validate_presence_of :body }
-  it { should validate_presence_of :name }
+  describe 'validations' do
+    it { should validate_uniqueness_of :body }
+    it { should validate_uniqueness_of :name }
+    it { should validate_presence_of :body }
+    it { should validate_presence_of :name }
+  end
+  describe 'custom methods' do
+    let(:response_double) { double("response", body: youtube, headers: youtube_headers) }
+    before(:each) do
+      allow(RestClient).to receive(:get).and_return(response_double.body)
+      RestClient.stub_chain(:get, :headers).and_return(response_double.headers)
+    end
+    describe '#get_youtube_channel' do
+      it 'stores youtube channel info in the database' do
+        expect(ApiResponse.get_youtube_channel).to eq(response_double.body)
+      end
+    end
+  end
 end
